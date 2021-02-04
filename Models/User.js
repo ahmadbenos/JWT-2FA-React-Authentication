ce8e07d9cf74f57b8a6a696b2694afefb31e05ed
Ahmad <benos.tarabain@gmail.com>
2021-02-04 02:22:05 +0200

    test

--- Models/User.js
+++ Models/User.js
@@ -1,7 +1,4 @@
 const mongoose = require("mongoose");
-const dayjs = require("dayjs");
-const LocalizedFormat = require("dayjs/plugin/localizedFormat");
-dayjs.extend(LocalizedFormat);
 
 const User = new mongoose.Schema({
   id: { type: String },
