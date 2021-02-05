3a55376e2cc172b687d1c583ecbc361ade772a5e
Ahmad <benos.tarabain@gmail.com>
2021-02-05 02:12:20 +0200

    spent the night testing out specific parts of the context API, for now i completed the register page

--- controllers/register.js
+++ controllers/register.js
@@ -7,12 +7,16 @@ exports.registerUser = async (req, res) => {
 
   // check for password and email validation input
   if (!password || !email) {
-    return res.status(400).json({ msg: "Invalid email/password input!" });
+    return res
+      .status(400)
+      .json({ msg: "Invalid email/password input!", status: "error" });
   }
   // Check if user already exists
   const checkedUser = await User.findOne({ email });
   if (checkedUser !== null) {
-    return res.status(400).json({ msg: "User already exists!" });
+    return res
+      .status(400)
+      .json({ msg: "User already exists!", status: "error" });
   }
 
   bcrypt.genSalt(10, (err, salt) => {
@@ -27,10 +31,12 @@ exports.registerUser = async (req, res) => {
       //? save the user to database
       try {
         await newUser.save();
-        res.status(200).json({ msg: "Success registeration" });
+        res
+          .status(200)
+          .json({ msg: "Success registeration", status: "success" });
       } catch (err) {
         console.log(err);
-        res.status(400).json({ msg: "Failure Registeration" });
+        res.status(400).json({ msg: "Failure Registeration", status: "error" });
       }
     });
   });
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +17,7 @@ app.get("/api", (req, res) => {
 });
 
 app.use("/api/register", require("./routes/register"));
+app.use("/api/login", require("./routes/login"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
